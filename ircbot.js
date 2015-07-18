9db5078af15a246be643e9988927650c6098bc4c
alexdor <aldor94@gmail.com>
2015-07-18 04:28:35 +0300

    Add regex instead of arrays

--- ircbot.js
+++ ircbot.js
@@ -1,29 +1,20 @@
 module.exports = {
   // Reply with the status of TechMinistry when someone says "ConsuelaTM, status"
   containsNameAndStatus: function(message, numOfHackers) {
-    var msg = message.toLowerCase().split(" ");
+    var msg = message.toLowerCase();
     var containsName = false;
     var containStatus = false;
-    var names = ["consuelatm", "consuelatm,", "consuelatm:", "consuela", "consuela,", "consuela:", "κονσουέλα", "κονσουελα", "κονσουέλα,", "κονσουελα,", "κονσουέλα:", "κονσουελα:", "ψονσθελα"];
-    var status = ["status", "στάτους", "στατους", "στατθσ", "στατυς", "katastasi", "katastash", "στάτυς", "κατάσταση", "κατασταση",
-      "anoixtos", "anixtos", "ανοιχτός", "ανοιχτος", "xwros", "xoros", "χωρος", "χώρος"
-    ];
-    for (var i = 0; i < msg.length; i++) {
-      if (names.indexOf(msg[i]) > -1) {
-        containsName = true;
-      } else if (status.indexOf(msg[i]) > -1) {
-        containStatus = true;
-      }
-      if (containsName && containStatus) {
-        if (numOfHackers == 0) {
-          return ', sorry space is closed'
-        } else if (numOfHackers == 1) {
-          return ', there is one hacker at the moment'
-        } else if (numOfHackers > 1) {
-          return ', there are ' + numOfHackers + ' hackers at the moment'
-        } else {
-          return ', sorry I couldn\'t get the information you asked for, please try again in a few minutes'
-        }
+    var names = /(consuela|κονσου(έ|ε)λα)+/ig;
+    var status = /(status|στ(α|ά)τ(ου|υ|θ)(σ|ς)|κατ(α|ά)σταση|χ(ώ|ω)ρο(ς|υ)?|x(w|o)ro(s|u)?|katastas(i|h)|an(i|oi|h)xtos|ανοιχτ(ο|ό)ς)+/ig;
+    if (names.test(msg) && status.test(msg)) {
+      if (numOfHackers == 0) {
+        return ', sorry space is closed with milk'
+      } else if (numOfHackers == 1) {
+        return ', there is one hacker at the moment'
+      } else if (numOfHackers > 1) {
+        return ', there are ' + numOfHackers + ' hackers at the moment'
+      } else {
+        return ', sorry I couldn\'t get the information you asked for, please try again in a few minutes'
       }
     }
   }
